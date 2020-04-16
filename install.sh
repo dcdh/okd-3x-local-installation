12400838355ef12154425d72b73c1f61a1e09620
dcdh <damien.clementdhuart@gmail.com>
2020-04-17 00:39:34 +0200

    add necessary packages

--- install.sh
+++ install.sh
@@ -7,3 +7,12 @@ then
   exit 1
 fi
 
+# install necessary package
+sudo yum install qemu-kvm libvirt libvirt-python libguestfs-tools virt-install -y && \
+  sudo yum install ansible python3 -y && \
+  # Update / Install needed package for qemu following fail to use fw_cfg https://www.spinics.net/lists/centos/msg166715.html
+  sudo yum install centos-release-qemu-ev qemu-kvm-ev -y
+
+# enable and start libvirt
+sudo systemctl enable libvirtd && \
+  sudo systemctl start libvirtd
