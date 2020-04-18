886cf72c6ddfdcf2ef1447295e07d3aaceb896e6
dcdh <damien.clementdhuart@gmail.com>
2020-04-18 03:44:52 +0200

    add dns virtual machine. Get fucked by jinja2 template generator, dns cloud init setup (misunderstood resolv.conf first setup to resolve mirror.centos when adding packages also I add to reference modules to load in cloud_config_modules).

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo yum install ansible -y
-
 # TODO use command line to retrieve ansible_become_password
 
 ansible-playbook install_okd.yaml --extra-vars "ansible_become_password=damien"
-
