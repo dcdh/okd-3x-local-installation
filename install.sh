c782e80ed2ff7e980c0712ac14b70a8c7f66329f
dcdh <damien.clementdhuart@gmail.com>
2020-04-20 01:19:52 +0200

    go to roles :) + some fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# TODO use command line to retrieve ansible_become_password
-
-ansible-playbook install_okd.yaml --extra-vars "ansible_become_password=damien"
+ansible-playbook install_okd.yml
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# TODO use command line to retrieve ansible_become_password
-
-ansible-playbook uninstall_okd.yaml --extra-vars "ansible_become_password=damien"
+ansible-playbook uninstall_okd.yml
