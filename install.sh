c43b3ead1bf6d7580b11b66bacd59ae4ab495857
dcdh <damien.clementdhuart@gmail.com>
2020-04-26 20:58:33 +0200

    add installation script execution

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 ansible-playbook install_okd.yml --ask-become-pass
+
+
+# TODO dynamic
+
+ansible-playbook -i /home/damien/.okd/vms/master.okd.local/openshift-ansible/inventory.ini /home/damien/.okd/vms/master.okd.local/openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook -i /home/damien/.okd/vms/master.okd.local/openshift-ansible/inventory.ini /home/damien/.okd/vms/master.okd.local/openshift-ansible/playbooks/deploy_cluster.yml
+
+ssh damien@master.okd.local 'oc adm policy add-cluster-role-to-user cluster-admin damien'
